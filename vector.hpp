098334abaf625cc82c9856ac6490a9b78d359a67
asalek <ayoub.salek8599@gmail.com>
2022-10-16 17:20:35 +0100

    add: gitignore file

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: asalek <asalek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 12:50:30 by asalek            #+#    #+#             */
-/*   Updated: 2022/10/16 15:28:25 by asalek           ###   ########.fr       */
+/*   Updated: 2022/10/16 16:15:38 by asalek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,20 @@ namespace ft
 	template <typename T, class Allocator=allocator<T> >
 	class Vector
 	{
-		typedef	T	value_type;
-		typedef allocator<T>	alloc;
-		typedef	T*	pointer;
+		typedef	T						value_type;
+		typedef allocator<value_type>	alloc;
+		typedef	T*						pointer;
+		const T*						const_pointer;
+		size_t							size_type;
+		value_type						&reference;
+		// const	value_type				&const_reference;
 
 		public:
 		//member types
 			int	size;
 			Vector()
 			{
-				pointer = alloc.allocate(3);
+				// pointer = alloc.allocate(3);
 				// alloc.deallocate(pointer, 3);
 				size = 3;
 			};

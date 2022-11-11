21fbc4113eb8f81ff6e3b2c7d7140616680a96d1
asalek <ayoub.salek8599@gmail.com>
2022-11-11 21:26:34 +0100

    add & update: add Stack size function and update stack tests

--- stack.cpp
+++ stack.cpp
@@ -7,9 +7,14 @@ int main()
 	ft::Vector<int> myvector (2,200);        // vector with 2 elements
 
 	ft::stack<int> first;                    // empty stack
-	if(first.empty())
+	ft::stack<int> second (myvector);         // stack initialized to copy of deque
+	
+	//empty test
+	cout << "Stack Second empty ? :";
+	if(second.empty())
 		cout <<"YES\n";
 	else
 		cout <<"NO\n";
-	// ft::stack<int> second (myvector);         // stack initialized to copy of deque
+	//size test
+	cout << "size of second Stack : " << second.size() << endl;
 }
--- stack.hpp
+++ stack.hpp
@@ -8,10 +8,11 @@ namespace ft
 	{
 		typedef T			value_type;
 		typedef Container	container_type;
-		size_t				size_type;
+		typedef size_t		size_type;
 		container_type		c;
 		public:
 			explicit stack (const container_type& ctnr = container_type()) : c(ctnr) {};
-			bool	empty()  {return c.empty(); }
+			bool	empty()  { return c.empty(); }
+			size_type size() const { return c.size(); }
 	};
 }

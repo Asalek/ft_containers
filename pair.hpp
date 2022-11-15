374233fb877e63d2657685f46b360d93befb0c0d
asalek <ayoub.salek8599@gmail.com>
2022-11-15 22:57:35 +0100

    update: pair class overload operators

--- pair.hpp
+++ pair.hpp
@@ -30,7 +30,28 @@ namespace ft
 	};
 	template <class T1, class T2>
 	pair<T1, T2> make_pair (T1 x, T1 y)
-	{
-		return ( pair<T1, T2>(x, y); );
-	}
+	{ return ( pair<T1, T2>(x, y); ); }
+	template <class T1, class T2>
+	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first==rhs.first && lhs.second==rhs.second; }
+
+	template <class T1, class T2>
+	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs==rhs); }
+
+	template <class T1, class T2>
+	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second); }
+
+	template <class T1, class T2>
+	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(rhs<lhs); }
+
+	template <class T1, class T2>
+	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return rhs<lhs; }
+
+	template <class T1, class T2>
+	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs<rhs); }
 }

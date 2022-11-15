2db6ac14adbc8c9bfdfbc4388cfacc36406c39eb
asalek <ayoub.salek8599@gmail.com>
2022-11-15 23:01:06 +0100

    update: use ft::pair instetad of std::pair

--- map.hpp
+++ map.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
+#include "pair.hpp"
 #include <functional>
+
 namespace ft
 {
 	template <class Key,
 				class T,
 				class Compare = std::less<Key>,
-				class Alloc = std::allocator<std::pair<const Key,T> > >
+				class Alloc = std::allocator<ft::pair<const Key,T> > >
 	class map
 	{
 		private:

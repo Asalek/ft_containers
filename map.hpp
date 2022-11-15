4ad21d4804b4175e2173ce27d7d2f2876123dbbb
asalek <ayoub.salek8599@gmail.com>
2022-11-15 22:30:10 +0100

    fix & update: fix ';' in stack and add map template

---
+++ map.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <functional>
+namespace ft
+{
+	template <class Key,
+				class T,
+				class Compare = std::less<Key>,
+				class Alloc = std::allocator<std::pair<const Key,T> > >
+	class map
+	{
+		private:
+			
+		public:
+			
+	};
+}
--- stack.hpp
+++ stack.hpp
@@ -28,7 +28,7 @@ namespace ft
 	template <class T, class Container>
 	bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
 	{
-		return (lhs.c != rhs.c)
+		return (lhs.c != rhs.c);
 	}
 	template <class T, class Container>
 	bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs)

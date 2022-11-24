c109ca0a81066e70649fea8ec60a053a671c6663
asalek <ayoub.salek8599@gmail.com>
2022-11-24 16:57:30 +0100

    update: insert function in redBlackTree

--- redBlackTree.hpp
+++ redBlackTree.hpp
@@ -45,31 +45,68 @@ namespace ft
 				return nodeCreated;
 			}
 		public:
-
+			node_pointer	find(const value_type &data) const
+			{
+				node_pointer current = this->root;
+				while (current != nullptr)
+				{
+					if (!_comp(current->data, data) && !_comp(data, current->data))
+						break ; //!(data < c.data) && !(c.data < data) => data = c.data
+					current = _comp(current->data, data) ? current->right : current->left;
+				}
+				return current;
+			}
 			//	insertion
 			void	insert(value_type data = value_type())
 			{
-				if (this->root == nullptr)
+				if (this->root == nullptr) //if !root make it
 				{
+					this->root = makenode(data);
+					root->parent = this->end;
+					this->end->left = this->root;
+					// this->end->right = this->root;
 					this->size++;
+					return ;
 				}
+				if (find(data) != nullptr) // if data exist don't insert it
+					return ;
+				this->end->left = nullptr;//remove it later
+				root->parent = nullptr;
+				node_pointer new_node = makenode(data);
+				node_pointer parent = nullptr;
+				node_pointer node_root = this->root;
+				while (node_root != nullptr) // go to data position
+				{
+					parent = node_root;
+					node_root = (_comp(new_node->data, node_root->data)) ? node_root->left : node_root->right;
+					new_node->parent = parent;
+				}
+				if (_comp(new_node->data, node_root->data)) //  newNode goes left or right
+					parent->left = new_node;
+				else
+					parent->right = new_node;
+				new_node->color = RED;
+				this->size++;
+				this->end->left = this->root;
+				this->root->parent = this->end;
+				//fixAfterInsert(new_node);
 			}
 	};
 
 	int main()
 	{
 		RedBlackTree<int> bst;
-		bst.insert(55);
-		bst.insert(40);
-		bst.insert(65);
-		bst.insert(60);
-		bst.insert(75);
-		bst.insert(57);
+		// bst.insert(55);
+		// bst.insert(40);
+		// bst.insert(65);
+		// bst.insert(60);
+		// bst.insert(75);
+		// bst.insert(57);
 
-		bst.printTree();
-		cout << endl
-			<< "After deleting" << endl;
-		bst.deleteNode(40);
-		bst.printTree();
+		// bst.printTree();
+		// cout << endl
+		// 	<< "After deleting" << endl;
+		// bst.deleteNode(40);
+		// bst.printTree();
 	}
 }

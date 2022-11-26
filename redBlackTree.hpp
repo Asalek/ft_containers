f59c5ea8660a1a5bb78d811af89ec2862c7f6962
asalek <ayoub.salek8599@gmail.com>
2022-11-26 19:24:07 +0100

    add: add left and right rotate

--- redBlackTree.hpp
+++ redBlackTree.hpp
@@ -154,6 +154,38 @@ namespace ft
 				}
 				this->root->color = BLACK;
 			}
+			void	leftRotate(node_pointer x)
+			{
+				node_pointer y = x->right;
+				x->right = y->left;
+				if (y->left != nullptr)
+					y->left->parent = x;
+				y->parent = x->parent;
+				if (x->parent == nullptr)
+					this->root = y;
+				else if (x == x->parent->left)
+					x->parent->left = y;
+				else
+					x->parent->right = y;
+				y->left = x;
+				x->parent = y;
+			}
+			void rightRotate(node_pointer x)
+			{
+				node_pointer y = x->left;
+				x->left = y->right;
+				if (y->right != nullptr)
+					y->right->parent = x;
+				y->parent = x->parent;
+				if (x->parent == nullptr)
+					this->root = y;
+				else if (x == x->parent->right)
+					x->parent->right = y;
+				else
+					x->parent->left = y;
+				y->right = x;
+				x->parent = y; 
+			}
 	};
 
 	int main()

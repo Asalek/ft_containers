c3aa976fa41875d578d9ba551f7c4b1cf3a08998
asalek <ayoub.salek8599@gmail.com>
2022-11-30 23:10:13 +0100

    rename & update : rename redBlackTree to .cpp for testing purpose, and add erase function and it's dependencies

--- redBlackTree.cpp
+++ redBlackTree.cpp
@@ -1,14 +1,15 @@
-#pragma once
+// #pragma once
 
 #define BLACK 1
 #define RED 0
 #include <iostream>
+#include "iterators.hpp"
 using std::cout;
 using std::endl;
 using std::string;
 
-namespace ft
-{
+// namespace ft
+// {
 	// Implementing Red-Black Tree in C++
 	template<class T>
 	struct node {
@@ -45,6 +46,42 @@ namespace ft
 				return nodeCreated;
 			}
 		public:
+		RedBlackTree(): root(nullptr), end(nullptr), size(0), _comp()
+		{
+			end = this->makenode();
+		}
+		RedBlackTree(value_compare _comp, allocator_type _alloc): root(nullptr), end(), _alloc(_alloc), _comp(_comp), size(0)
+		{
+			end = this->makenode();
+		}
+
+		RedBlackTree(const RedBlackTree& _tr) : root(nullptr), end(), _alloc(_tr._alloc), _comp(), size(0)
+		{
+			*this = _tr;
+		}
+			void printHelper(node_pointer root, std::string indent, bool last) {
+        	// print the tree structure on the screen
+            if (root != nullptr) {
+                std::cout << indent;
+                if (last) {
+                    std::cout<<"R----";
+                    indent += "     ";
+                } else {
+                    std::cout<<"L----";
+                    indent += "|    ";
+                }
+
+                std::cout<<root->data<<"( C = "<<root->color<<")"<<std::endl;
+
+                printHelper(root->left, indent, false);
+                printHelper(root->right, indent, true);
+            }
+        }
+					void printTree() {
+				if (root) {
+				printHelper(this->root, "", true);
+				}
+			}
 			node_pointer	find(const value_type &data) const
 			{
 				node_pointer current = this->root;
@@ -81,7 +118,7 @@ namespace ft
 					node_root = (_comp(new_node->data, node_root->data)) ? node_root->left : node_root->right;
 					new_node->parent = parent;
 				}
-				if (_comp(new_node->data, node_root->data)) //  newNode goes left or right
+				if (_comp(new_node->data, parent->data)) //  newNode goes left or right
 					parent->left = new_node;
 				else
 					parent->right = new_node;
@@ -116,7 +153,7 @@ namespace ft
 						and left rotate newNode (f)	*/
 						else
 						{
-							if (f = f->parent->right)
+							if (f == f->parent->right)
 							{
 								f = f->parent;
 								leftRotate(f);
@@ -193,22 +230,64 @@ namespace ft
 				y->right = x;
 				x->parent = y; 
 			}
+			void	transplant(node_pointer del, node_pointer child) // emplace nodeToBeDeleted with it's children's
+			{
+				if (del->parent == nullptr)
+					this->root = child;
+				else if (del == del->parent->left)
+					del->parent->left = child;
+				else
+					del->parent->right = child;
+				if (child != nullptr)
+					child->parent = del->parent;
+			}
+
+			void	deleteNode(value_type value)
+			{
+				node_pointer Del;
+				node_pointer x;
+
+				if (!root || (Del = find(value)) == nullptr)
+					return ;
+				//Save the color of nodeToBeDeleted in origrinalColor
+				bool originalColor = Del->color;
+				//If the left child of Deleted is NULL, Assign the right child of Deleted to x
+				if (Del->left == nullptr)
+				{
+					x = Del->right;
+					transplant(Del, Del->right);
+					_alloc.destroy(Del);
+					_alloc.deallocate(Del, 1);
+				}
+				else if (Del->right == nullptr)
+				{
+					x = Del->left;
+					transplant(Del, Del->left);
+					_alloc.destroy(Del);
+					_alloc.deallocate(Del, 1);
+				}
+				else
+				{
+					
+				}
+			}
 	};
 
 	int main()
 	{
 		RedBlackTree<int> bst;
-		// bst.insert(55);
-		// bst.insert(40);
-		// bst.insert(65);
-		// bst.insert(60);
-		// bst.insert(75);
-		// bst.insert(57);
+		bst.insert(55);
+		bst.insert(40);
+		bst.insert(65);
+		bst.insert(60);
+		bst.insert(75);
+		bst.insert(57);
 
-		// bst.printTree();
-		// cout << endl
-		// 	<< "After deleting" << endl;
-		// bst.deleteNode(40);
-		// bst.printTree();
+		bst.printTree();
+		cout << endl
+			<< "After deleting" << endl;
+		bst.deleteNode(40);
+		bst.printTree();
+		return 0;
 	}
-}
+// }

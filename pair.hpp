a568e1b873d8728c27208a2c19a56d092a5deb46
asalek <ayoub.salek8599@gmail.com>
2022-12-12 19:31:02 +0100

    fix: switch private methods to public

--- pair.hpp
+++ pair.hpp
@@ -5,28 +5,29 @@ namespace ft
 	template< class T1, class T2>
 	class pair
 	{
-		typedef T1	first_type;
-		typedef T2	second_type;
-		first_type	first;
-		second_type	second;
-		pair():first(), second(){}
-		template<class U, class V>
-		pair (const pair<U,V>& pr)
-		{
-			this->first = pr.first;
-			this->second = pr.second;
-		}
-		pair (const first_type& a, const second_type& b)
-		{
-			this->first = a;
-			this->second = b;
-		}
-		pair& operator= (const pair& pr)
-		{
-			this->first = pr.first;
-			this->second = pr.second;
-			return *this;
-		}
+		public:
+			typedef T1	first_type;
+			typedef T2	second_type;
+			first_type	first;
+			second_type	second;
+			pair():first(), second(){}
+			template<class U, class V>
+			pair (const pair<U,V>& pr)
+			{
+				this->first = pr.first;
+				this->second = pr.second;
+			}
+			pair (const first_type& a, const second_type& b)
+			{
+				this->first = a;
+				this->second = b;
+			}
+			pair& operator= (const pair& pr)
+			{
+				this->first = pr.first;
+				this->second = pr.second;
+				return *this;
+			}
 	};
 	template <class T1, class T2>
 	pair<T1, T2> make_pair (T1 x, T1 y)
